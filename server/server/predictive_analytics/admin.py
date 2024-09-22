82946c44e5c63f69b878c5dd8efed731884a87be
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 15:44:42 +0300

    Updated API endpoints to request predictions and created Admin interface for managing predictions

--- server/server/predictive_analytics/admin.py
+++ server/server/predictive_analytics/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import EnergyPrediction
+"""
+Admin configuration for the EnergyPrediction model.
+"""
 
-# Register your models here.
+@admin.register(EnergyPrediction)
+class EnergyPredictionAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the EnergyPrediction model.
+    """
+    list_display = ('user', 'timestamp', 'predicted_consumption', 'actual_consumption')
+    list_filter = ('user', 'timestamp')
+    search_fields = ('user__username',)
--- server/server/urls.py
+++ server/server/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from energy_data.views import EnergyConsumptionViewSet, SmartMeterViewSet
+from predictive_analytics.views import EnergyPredictionViewSet
+"""
+URLs for the energy data API.
+"""
+
+router = DefaultRouter()
+router.register(r'energy_consumption', EnergyConsumptionViewSet)
+router.register(r'smart_meter', SmartMeterViewSet)
+router.register(r'energy_prediction', EnergyPredictionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
