a5276754be212a3da3e4a95808b64103b2b5bc6d
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 14:14:32 +0300

    Setup url for energy_data app

---
+++ server/server/energy_data/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from energy_data.views import EnergyConsumptionViewSet, SmartMeterViewSet
+"""
+URLs for the energy data API.
+"""
+
+router = DefaultRouter()
+router.register(r'energy_consumption', EnergyConsumptionViewSet)
+router.register(r'smart_meter', SmartMeterViewSet)
+
+urlpatterns = [
+  path('admin/', admin.site.urls),
+  path('api/', include(router.urls)),
+]
--- server/server/urls.py
+++ server/server/urls.py
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from energy_data.views import EnergyConsumptionViewSet, SmartMeterViewSet
-"""
-URLs for the energy data API.
-"""
-
-router = DefaultRouter()
-router.register(r'energy-consumption', EnergyConsumptionViewSet)
-router.register(r'smart-meters', SmartMeterViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
