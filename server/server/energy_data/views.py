510d1ece969bf7b2a584625d133c4929c450a181
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 13:47:12 +0300

    Created basic view for collecting energy consumption data

--- server/server/energy_data/views.py
+++ server/server/energy_data/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import EnergyConsumption, SmartMeter
+from .serializers import EnergyConsumptionSerializer, SmartMeterSerializer
+"""
+Views for the energy data API.
+"""
 
-# Create your views here.
+
+class EnergyConsumptionViewSet(viewsets.ModelViewSet):
+  """
+  A viewset for the EnergyConsumption model.
+  """
+  queryset = EnergyConsumption.objects.all()
+  serializer_class = EnergyConsumptionSerializer
+
+  def create(self, request, *args, **kwargs):
+    """
+    Create a new energy consumption record.
+    """
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    self.perform_create(serializer)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # headers = self.get_success_headers(serializer.data)
+
+class SmartMeterViewSet(viewsets.ModelViewSet):
+  """
+  A viewset for the SmartMeter model.
+  """
+  queryset = SmartMeter.objects.all()
+  serializer_class = SmartMeterSerializer
+
+  def create(self, request, *args, **kwargs):
+    """
+    Create a new smart meter record.
+    """
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    self.perform_create(serializer)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # headers = self.get_success_headers(serializer.data)
