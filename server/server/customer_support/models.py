698b1e40f158e0606f782bddb2b3645bdb48f6ec
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 16:02:15 +0300

    Updated customer_support model

--- server/server/customer_support/models.py
+++ server/server/customer_support/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+"""
+Module to store chat sessions and messages.
+"""
 
-# Create your models here.
+
+class ChatSession(models.Model):
+  """
+  A model to store chat sessions.
+  """
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  session_id = models.CharField(max_length=100, unique=True)
+  start_time = models.DateTimeField(auto_now_add=True)
+  end_time = models.DateTimeField(null=True, blank=True)
+
+class ChatMessage(models.Model):
+  """
+  A model to store chat messages.
+  """
+  session = models.ForeignKey(ChatSession, on_delete=models.CASCADE)
+  sender = models.CharField(max_length=100)
+  message = models.TextField()
+  timestamp = models.DateTimeField(auto_now_add=True)
