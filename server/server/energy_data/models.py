0ea6a39a1a178c06fce82b7daa909dde0959e58a
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 13:40:16 +0300

    Added basic model for energy consumption

--- server/server/energy_data/models.py
+++ server/server/energy_data/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class EnergyConsumption(models.Model):
+  """
+  A model representing the energy consumption data for a user.
+  """
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  timestamp = models.DateTimeField()
+  consumption = models.FloatField()
+
+  class Meta:
+    """
+    Meta options for the EnergyConsumption model.
+    """
+    ordering = ['-timestamp']
+
+class SmartMeter(models.Model):
+  """
+  A model representing a smart meter.
+  """
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  meter_id = models.CharField(max_length=100, unique=True)
+  last_reading = models.DateTimeField(null=True, blank=True)
