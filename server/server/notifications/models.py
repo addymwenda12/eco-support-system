f516615cdc02ea69131ea06adda55e78ccb3eab2
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 20:48:34 +0300

    Updated notifications model

--- server/server/notifications/models.py
+++ server/server/notifications/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+"""
+Models for the notifications app.
+"""
 
-# Create your models here.
+
+class Notification(models.Model):
+    """
+    A model to store notifications for users.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    is_sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        """
+        Return a string representation of the notification.
+        """
+        return f"Notification for {self.user.username} at {self.sent_at}"
