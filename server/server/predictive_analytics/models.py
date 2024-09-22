457a5b9ff77315e0388267ba4e6c259fe7801cef
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 15:24:29 +0300

    Created models for predictive analytics

--- server/server/predictive_analytics/models.py
+++ server/server/predictive_analytics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+"""
+Module to store energy consumption predictions.
+"""
 
-# Create your models here.
+class EnergyPrediction(models.Model):
+  """
+  A model to store energy consumption predictions.
+  """
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  predicted_consumption = models.FloatField()
+  actual_consumption = models.FloatField(null=True, blank=True)
+  # meter = models.ForeignKey('energy_data.SmartMeter', on_delete=models.CASCADE)
+
+  def __str__(self):
+    """
+    Return a string representation of the EnergyPrediction model.
+    """
+    return f"{self.user.username} - {self.timestamp} - Predicted: {self.predicted_consumption}, Actual: {self.actual_consumption} kWh"
+
