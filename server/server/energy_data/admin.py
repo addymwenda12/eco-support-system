b7afc37d5e6882f37ddd468402398ec0ed3ae499
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 14:09:10 +0300

    Added basic admin functionalities

--- server/server/energy_data/admin.py
+++ server/server/energy_data/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import EnergyConsumption, SmartMeter
+"""
+Admin classes for the energy data models.
+"""
 
-# Register your models here.
+@admin.register(EnergyConsumption)
+class EnergyConsumptionAdmin(admin.ModelAdmin):
+    """
+    Admin class for the EnergyConsumption model.
+    """
+    list_display = ('user', 'timestamp', 'consumption', 'meter', 'cost')
+    list_filter = ('user', 'meter', 'timestamp')
+    search_fields = ('user__username', 'meter__meter_id')
+
+@admin.register(SmartMeter)
+class SmartMeterAdmin(admin.ModelAdmin):
+    """
+    Admin class for the SmartMeter model.
+    """
+    list_display = ('meter_id', 'user', 'last_reading', 'is_active', 'installation_date')
+    list_filter = ('is_active', 'installation_date')
+    search_fields = ('meter_id', 'user__username')
+
