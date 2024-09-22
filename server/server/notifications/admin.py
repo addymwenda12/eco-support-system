74b6563fca336c5e9ef95b99cb0bc76c263875b1
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 21:30:18 +0300

    Updated admin for notifications

--- server/server/notifications/admin.py
+++ server/server/notifications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'sent_at', 'is_sent')
+    list_filter = ('is_sent', 'sent_at')
+    search_fields = ('user__username', 'message')
+
