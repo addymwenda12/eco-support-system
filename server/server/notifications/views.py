dbba7a6fc509a2816fa0ee387baaf6a8f01a8af8
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 21:17:15 +0300

    Updated views for notification

--- server/server/notifications/views.py
+++ server/server/notifications/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Notification
+from .serializers import NotificationSerializer
+from .sms_service import send_sms
 
-# Create your views here.
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    """
+
+    """
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    @action(detail=False, methods=['post'])
+    def send_notification(self, request):
+        """
+
+        """
+        user = request.user
+        message = request.data.get('message')
+        phone_number = user.profile.phone_number
+
+        if not message:
+            return Response({'error': 'Message is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        notification = Notification.objects.create(user=user, message=message)
+        sms_response = send_sms(phone_number, message)
+
+        if sms_response:
+            notification.is_sent = True
+            notification.save()
+            return Response({'status': 'Notification sent successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Failed to send notification'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
