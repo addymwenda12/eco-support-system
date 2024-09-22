e97edbc0b81ddf5f6f817d9aa688e355eb15b66a
addymwenda12 <muneneaddy@gmail.com>
2024-09-22 13:58:19 +0300

    Updated models to include meter and cost fields

--- server/server/energy_data/models.py
+++ server/server/energy_data/models.py
@@ -8,12 +8,20 @@ class EnergyConsumption(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
   consumption = models.FloatField()
+  meter = models.ForeignKey('SmartMeter', on_delete=models.CASCADE)
+  cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
   class Meta:
     """
     Meta options for the EnergyConsumption model.
     """
     ordering = ['-timestamp']
+
+  def __str__(self):
+    """
+    Return a string representation of the EnergyConsumption model.
+    """
+    return f"{self.user.username} - {self.timestamp} - {self.consumption} kWh"
 
 class SmartMeter(models.Model):
   """
@@ -22,3 +30,11 @@ class SmartMeter(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   meter_id = models.CharField(max_length=100, unique=True)
   last_reading = models.DateTimeField(null=True, blank=True)
+  is_active = models.BooleanField(default=True)
+  installation_date = models.DateField()
+
+  def __str__(self):
+    """
+    Return a string representation of the SmartMeter model.
+    """
+    return f"Meter {self.meter_id} - {self.user.username}"
